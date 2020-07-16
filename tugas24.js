0ef00720cccb1ed527742e5d22f8eca41f550805
hugoiman <hugogiman17@gmail.com>
2020-07-16 12:40:20 +0700

    Tugas 24 Javascript

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+var number = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log("Sebelumnya : ", number);
+console.log("Ascending : ", number.sort());
+console.log("Descending : ", number.reverse());
